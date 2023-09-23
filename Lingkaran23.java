1ac4b66935f8fc2ad31c2db8b738aaaefaeb0a1c
PurnamaRidzkyN <143416997+PurnamaRidzkyN@users.noreply.github.com>
2023-09-23 17:26:26 +0700

    Update Lingkaran23.java

--- Lingkaran23.java
+++ Lingkaran23.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
+
 public class Lingkaran23 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System. in);
-        
+        Scanner input = new Scanner(System.in);
+
         int r;
         double luas, keliling;
+        double phi = 3.14;
 
         System.out.println("Masukan Jari-Jari Lingkaran: ");
         r = input.nextInt();
 
-        keliling = 2*3.14*r;
-        luas = 3.14*r*r;
+        keliling = 2 * phi * r; 
+        luas = phi * r * r; 
 
-        System.out.println(keliling);
-        System.out.println(luas);
-
-        
-
+        System.out.println("Keliling Lingkaran: " + keliling);
+        System.out.println("Luas Lingkaran: " + luas);
     }
 }
